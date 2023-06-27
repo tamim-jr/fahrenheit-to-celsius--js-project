dc98c9774bc5070fadedcff8cc1480d94ac0886d
tamim-jr <internationalinf24@gmail.com>
2023-06-27 12:18:17 +0600

    Fahrenheit to Celsius Calculetor

---
+++ app.js
@@ -0,0 +1,8 @@
+//fahrenheit to celsius method (32°F − 32) × 5/9 = 0°C 
+
+var farInput = parseFloat(prompt("Input your Farenheit value here: "));
+
+var output =  (farInput - 32) * 5/6;
+
+output = output.toFixed(2);
+document.write("Your Celsius value is: " + output);
